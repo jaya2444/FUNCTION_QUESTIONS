b8dc4c74f9251786750721dec7b4ff775b129148
jaya2444 <106364569+jaya2444@users.noreply.github.com>
2022-06-01 16:39:21 +0530

    Add files via upload

---
+++ D2.py
@@ -0,0 +1,19 @@
+# Write a Python function to find the Max of three numbers
+
+# def max_of_two( x, y ):
+#     if x > y:
+#         return x
+#     return y
+# def max_of_three( x, y, z ):
+#     return max_of_two( x, max_of_two( y, z ) )
+# print(max_of_three(3, 9, -5))
+
+# def my_function(a):
+#     i=0
+#     max=0
+#     while i<len(a):
+#         if a[i]>max:
+#             max=a[i]
+#         i=i+1
+#     print(max)
+# my_function([1,88,8])
---
+++ all_questions.py
@@ -0,0 +1,58 @@
+# duplicates remove
+
+# def duplicate(returns):
+#     a=[]
+#     for i in range(len(returns)):
+#         if returns[i] in a:
+#             pass
+#         else:
+#             a.append(returns[i])
+#     print(a)
+# duplicate([1,2,3,3,3,3,4,5])
+
+# def duplicates(remove):
+#     i=0
+#     a=[]
+#     while i<len(remove):
+#         if remove[i] in a:
+#             pass
+#         else:
+#             a.append(remove[i])
+#         i=i+1
+#     print(a)
+# duplicate([1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10])
+
+# def num(a):
+#     i=0
+#     while i<len(a):
+#         if a[i]%2==0:
+#             return a
+#         else:
+#             return a
+#         i=i+1
+# print(num([1,2,3,4,5,6,7]))
+
+#even number
+
+# def even(num):
+#     i=0
+#     while i<len(num):
+#         if i%2==0:
+#             print(i)
+#         i=i+1
+# even([1,2,3,4,5,6,7,8,9])
+
+# extra questions
+
+# def fun():
+#     for x in range(22,23,24):
+#         print(x)
+# fun()
+# op=2
+
+# def fun():
+#     return "str"+"char"
+# print(fun())
+# op=strchar
+
+
